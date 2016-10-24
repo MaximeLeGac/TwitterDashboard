644d79e04bdd68c836d89d8640102f1c1ed14002
vanbleusa <a.vanbleus@gmail.com>
2016-10-24 11:28:10 +0200

    COR controller REST

--- src/main/java/com/epsi/twitterdashboard/service/RestController.java
+++ src/main/java/com/epsi/twitterdashboard/service/RestController.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Produces;
 
 /**
  * @author Allan
- * REST Examples => https://www.mkyong.com/webservices/jax-rs/resteasy-hello-world-example/
  */
 @Path("rest")
 public class RestController {
@@ -22,34 +21,28 @@ public class RestController {
     @Path("/tweet/{id}")
     @Produces("application/json")
     /**
-     * Récupère le tweet correspondant à l'id en entrée
-     * Local        : http://localhost:8080/TwitterDashboard/dash/rest/tweet/{id}
-     * CleverCloud  : http://epsi-i4-twitterdashboard.cleverapps.io/rest/tweet/{id}
+     * Get tweet corresponding to the id 
      */
     public Tweet GetTweet(@PathParam("id") int id) {
         return new Tweet(Integer.toString(id));
-    }
-
-    @POST
-    @Path("/tweet")
-    @Consumes("application/json")
-    /**
-     * Ajoute un nouveau tweet
-     * Local        : http://localhost:8080/TwitterDashboard/dash/rest/tweet
-     * CleverCloud  : http://epsi-i4-twitterdashboard.cleverapps.io/rest/tweet/
-     */
-    public void PostTweet(Tweet tweet) {
     }
 
     @GET
     @Path("/timeline")
     @Produces("application/json")
     /**
-     * Renvoi la timeline du compte configuré dans TwitterApi
-     * Local        : http://localhost:8080/TwitterDashboard/dash/rest/timeline/
-     * CleverCloud  : http://epsi-i4-twitterdashboard.cleverapps.io/rest/timeline/
+     * Get current user timeline
      */
     public List GetTimeline() throws TwitterException {
         return RestTwitterApi.GetInstance().getHomeTimeline();
+    }
+
+    @POST
+    @Path("/tweet")
+    @Consumes("application/json")
+    /**
+     * Add a new tweet
+     */
+    public void PostTweet(Tweet tweet) {
     }
 }
