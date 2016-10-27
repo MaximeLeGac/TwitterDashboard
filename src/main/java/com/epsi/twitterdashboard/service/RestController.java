e9d668c07e7c2753571f7aa790cd128924850919
vanbleusa <a.vanbleus@gmail.com>
2016-10-27 14:20:43 +0200

    MAJ URLs

--- src/main/java/com/epsi/twitterdashboard/service/RestController.java
+++ src/main/java/com/epsi/twitterdashboard/service/RestController.java
@@ -4,7 +4,9 @@ import com.epsi.twitterdashboard.utils.JsonFile;
 import com.epsi.twitterdashboard.twitter4j.RestTwitterApi;
 import java.io.IOException;
 import java.text.ParseException;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -18,9 +20,12 @@ import twitter4j.JSONException;
 public class RestController {
 
     @GET
-    @Path("/fetchtimeline/{username}")
+    @Path("/{username}/fetchtimeline")
     /**
      * Get specific user timeline
+     * @param username
+     * @param count
+     * @return
      */
     public String FetchTimeline(@PathParam("username") String username, @QueryParam("count") int count) throws IOException, JSONException, ParseException {
         JSONArray tweets = new JSONArray(RestTwitterApi.FetchTimeline(username, count));
@@ -31,25 +36,33 @@ public class RestController {
     @Path("/{username}/bookmarks")
     /**
      * Get user bookmarks
+     * @param username
+     * @return
      */
     public String GetBookmarks(@PathParam("username") String username) {
         JSONArray bookmarks = new JSONArray(JsonFile.ReadBookmarks(username));
         return bookmarks.toString();
     }
 
-    @GET
+    @PUT
     @Path("/{username}/bookmark/{id}")
     /**
      * Bookmarks a tweet
+     * @param username
+     * @param id
+     * @return
      */
     public void Bookmark(@PathParam("username") String username, @PathParam("id") int id) {
         JsonFile.AddBookmark(username, id);
     }
 
-    @GET
-    @Path("/{username}/deletebookmark/{id}")
+    @DELETE
+    @Path("/{username}/bookmark/{id}")
     /**
      * Get specific user timeline
+     * @param username
+     * @param id
+     * @return
      */
     public void DeleteBookmark(@PathParam("username") String username, @PathParam("id") int id) {
         JsonFile.DeleteBookmark(username, id);
